563a51317400fd5924c7f2cd317319a941b70924
Rich Gilbank <richgilbank@gmail.com>
2013-11-25 20:05:16 -0500

    New config format

--- config.js
+++ config.js
@@ -1,11 +1,18 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah',
+  twit: {
+    consumer_key:         'blah',
+    consumer_secret:      'blah',
+    access_token:         'blah',
+    access_token_secret:  'blah'
+  },
 
-  owner_screen_name:    'blah',
-  slug:                 'blah'
+  twitter: {
+    owner_screen_name:    'blah',
+    slug:                 'blah'
+  },
+
+  wordnik: {
+    api_key:              'blah'
+  }
 }
-
